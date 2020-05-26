d7f9dbca7c41c8256c45eb9ae535117eb5236a7f
jjackinston <jjackinston@yahoo.com>
2020-05-26 13:45:04 -0400

    Ready to push the cachematrix file

--- cachematrix.R
+++ cachematrix.R
@@ -3,13 +3,32 @@
 
 ## Write a short comment describing this function
 
-makeCacheMatrix <- function(x = matrix()) {
-
+MakecacheMatrix <- function(x = vector()) {
+  f <- NULL
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  get <- function() x
+  set <- function(cache) m <<- cache
+  getmean <- function() m
+  list(set = set, get = get,
+       setmean = setmean,
+       getmean = getmean)
 }
 
 
 ## Write a short comment describing this function
 
+## Returning the vector
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  f <- x$getmean()
+  if(!is.null(f)) {
+    message("getting cached data")
+    return(f)
+  }
+  data <- x$get()
+  f <- mean(data, ...)
+  x$setmean(f)
+  f
 }
